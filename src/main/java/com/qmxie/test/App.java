be58c0ae1bf3a269e9f0f6e7e91225a7d6f2310d
qmxie <qmxie@thoughtworks.com>
2016-07-20 10:14:07 +0800

    [#0]init

---
+++ src/main/java/com/qmxie/test/App.java
@@ -0,0 +1,17 @@
+package com.qmxie.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static String getHello() {
+        return "Hello";
+    }
+}
